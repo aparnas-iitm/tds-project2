5ceb5dedd6376da7b6315394520e01d2f462230f
aparnas-iitm <21f3002580@ds.study.iitm.ac.in>
2025-03-31 22:21:59 +0530

    changed main.py

--- app/asyncio_path.py
+++
@@ -1,9 +0,0 @@
-import sys
-import types
-
-# Replace 'async' with 'async_' in the asyncio module
-if sys.version_info >= (3, 7):
-    import asyncio.tasks
-    if hasattr(asyncio.tasks, 'async'):
-        setattr(asyncio.tasks, 'async_', getattr(asyncio.tasks, 'async'))
-        delattr(asyncio.tasks, 'async')
--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
-import asyncio_path
+import sys
+import types
+
+# Replace 'async' with 'async_' in the asyncio module
+if sys.version_info >= (3, 7):
+    import asyncio.tasks
+    if hasattr(asyncio.tasks, 'async'):
+        setattr(asyncio.tasks, 'async_', getattr(asyncio.tasks, 'async'))
+        delattr(asyncio.tasks, 'async')
 
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
